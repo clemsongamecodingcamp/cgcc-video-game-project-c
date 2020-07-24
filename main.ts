d02a383677275648fa97cb0d8de45f8f9ec5c830
ctp432 <68613433+ctp432@users.noreply.github.com>
2020-07-24 15:20:50 -0400

    Sharpedo Bruce

--- main.ts
+++ main.ts
@@ -14,27 +14,24 @@ game.onGameUpdateWithHeading(function () {
     controller.moveSprite(Marlin, 50, 50)
     scene.cameraFollowSprite(Marlin)
 })
-function Start_Screen () {
-    game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
-}
 info.onCountdownEnd(function () {
     Bruce = sprites.create(img`
-        . . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . 
-        . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . . 
-        . . . . . . . . . . f c c b b c f . . . . . . . . . . . . c c c 
-        . . . . f f f f f f c c c c c c f f . . . . . . . . . c c b b c 
-        . . f f b b b b b b b b b b b b b c f f f . . . . . c d b b c . 
-        f f b b b b b b b b b c b c b b b b c c c f f . . c d d b b f . 
-        f b c b b b b b f f b b c b c b b b c c c c c f f f d b b f . . 
-        f b b b 1 1 1 1 f f 1 b c b c b b b c c c c c c c b b b c f . . 
-        . f b 1 1 1 1 1 1 1 1 b b b b b b c c c c c c c c c b c c f . . 
-        . . f c c c 3 3 c c 1 1 b b b b c c c c c c c c f f f b b c f . 
-        . . . f c 1 3 1 c 1 1 1 b b b c c c c c b d b c . . . f b b f . 
-        . . . . f 3 3 c 1 1 1 c b b b f d d d d d c c . . . . . f b b f 
-        . . . . . f f 1 1 1 1 f b d b b f d d c c . . . . . . . . f f f 
-        . . . . . . . c c c c c f b d b b f c . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . f f f f f . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . c c f 
+        . . . . . . . . . . . . c d d b 
+        . . . . . . . . . . . c d d b b 
+        . . . . . . . . . . f c c b b c 
+        . . . . f f f f f f c c c c c c 
+        . . f f b b b b b b b b b b b b 
+        f f b b b b b b b b b c b c b b 
+        f b c b b b b b f f b b c b c b 
+        f b b b 1 1 1 1 f f 1 b c b c b 
+        . f b 1 1 1 1 1 1 1 1 b b b b b 
+        . . f c c c 3 3 c c 1 1 b b b b 
+        . . . f c 1 3 1 c 1 1 1 b b b c 
+        . . . . f 3 3 c 1 1 1 c b b b f 
+        . . . . . f f 1 1 1 1 f b d b b 
+        . . . . . . . c c c c c f b d b 
+        . . . . . . . . . . . . . f f f 
         `, SpriteKind.Enemy)
     Bruce.setPosition(152, 20)
     Bruce.setVelocity(0, 50)
@@ -43,6 +40,9 @@ function bruceVelocity (num: number) {
     if (true) {
     	
     }
+}
+function Start_Screen () {
+    game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
 }
 let Bruce: Sprite = null
 let keyHave = 0
